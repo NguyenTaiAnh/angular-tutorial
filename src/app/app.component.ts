03f4d1a651178d030e2e228e98b2b0f6384adaa7
NguyenTaiAnh <anh.nguyen@tpisoftware.com>
2024-06-17 21:21:14 +0700

    day1 - data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+export interface User{
+  name:string;
+  age:number;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-tutorial';
+  title = VERSION.full;
+  user: User = {
+    name: "Anh Dev",
+    age: 18
+  };
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { RouterOutlet } from "@angular/router";
-import { AppComponent } from "./app.component";
-import { HomeComponent } from "./home/home.component";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterOutlet } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-    declarations:[AppComponent, HomeComponent],
-    imports:[CommonModule,RouterOutlet, BrowserModule,NzButtonModule],
-    bootstrap:[AppComponent],
+  declarations: [AppComponent, HomeComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    BrowserModule,
+    NzButtonModule,
+    FormsModule,
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
